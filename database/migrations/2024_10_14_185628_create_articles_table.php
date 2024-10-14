c03a2c4aa01110e59970449e539e1383a3a79f80
tahaazare <tahaazre.dev@gmail.com>
2024-10-14 22:33:45 +0330

    ARTICLE CATEGORY & ARTICLE [ MODEL , MIGRATION , RELATIONS ]

---
+++ database/migrations/2024_10_14_185628_create_articles_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+use function Laravel\Prompts\table;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('title')->unique();
+            $table->string('slug')->unique();
+
+            $table->foreignId('article_id')->constrained('article_categories')->onUpdate('cascade')->onDelete('cascade');
+            $table->foreignId('user_id')->constrained('users')->onUpdate('cascade')->onDelete('cascade');
+
+
+            $table->json('tags');
+
+            $table->text('description');
+            $table->tinyInteger('status')->default(1);
+
+            $table->text('image')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('articles');
+    }
+};
